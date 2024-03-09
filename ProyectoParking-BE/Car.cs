c8d9a42fc0e81d1caa48e8043b989cc5233ccadc
ivanBMA <97181101+ivanBMA@users.noreply.github.com>
2024-03-09 13:55:10 +0100

    Initial commit

---
+++ ProyectoParking-BE/Car.cs
@@ -0,0 +1,9 @@
+﻿namespace ProyectoParking_BE
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ ProyectoParking-BE/Controllers/CarsController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoParking_BE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        public CarsController()
+        {
+
+        }
+
+        [HttpGet("getById")]
+        public ActionResult<Car> GetById(int id)
+        {
+            var coches = AllCarsGet();
+            var coche = coches.Where(c => c.Id == id).FirstOrDefault();
+
+
+            return Ok(coche);
+        }
+
+        [HttpGet("getAllCars")]
+        public ActionResult<List<Car>> Index()
+        {
+            return Ok(AllCarsGet());
+        }
+
+        [HttpPost("postAddCar")]
+        public ActionResult<List<Car>> PostAddCar([FromBody]Car car)
+        {
+            var coches = AllCarsGet();
+            coches.Add(car);
+
+            return Ok(coches);
+        }
+
+        [HttpPut("putCar")]
+        public ActionResult<List<Car>> PutCar(int id, [FromBody] Car car)
+        {
+            var coches = AllCarsGet();
+            var cocheAntiguo = coches.Where(c => c.Id == id).FirstOrDefault();
+
+            cocheAntiguo.Name = car.Name;
+            cocheAntiguo.Description = car.Description;
+
+            return Ok(coches);
+        }
+
+        [HttpDelete("deleteById")]
+        public ActionResult<List<Car>> DeleteById(int id)
+        {
+            var coches = AllCarsGet();
+            var coche = coches.Where(c => c.Id == id).FirstOrDefault();
+
+            coches.Remove(coche);
+
+            return Ok(coches);
+        }
+
+        private List<Car> AllCarsGet()
+        {
+
+            Car car = new Car()
+            {
+                Id = 1,
+                Name = "Foo",
+                Description = "Bar",
+            };
+
+            Car car2 = new Car()
+            {
+                Id = 2,
+                Name = "Faa",
+                Description = "rapido",
+            };
+
+            List<Car> results = new List<Car>();
+            results.Add(car);
+            results.Add(car2);
+
+            return results;
+        }
+
+
+    }
+}
